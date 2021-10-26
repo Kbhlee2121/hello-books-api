0199855440d74564650c209e65a96fd9f735c701
Kbhlee2121 <Kbhlee2121@gmail.com>
2021-10-25 18:09:34 -0700

    Create book model

---
+++ app/models/book.py
@@ -0,0 +1,15 @@
+from app import db
+
+# like any other class, but creating a __init__ func is not necessary
+# can use the func that it inherits
+# can also create own class functions like any other class
+class Book (db.Model):
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    title = db.Column (db.String)
+    description = db.Column(db.String)
+    # SQLAlchemy prefers singular tablenames
+    # can change table name by this: 
+    # __tablename = "books"
+
+def to_string(self):
+    return f"{self.id}: {self.title} Description: {self.description}"
